d80c4e0a4bc7cd455e0c7fbfb113cc90330078a4
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-04-23 02:17:25 -0500

    Working Spotify Protocols. for Spotify This and Do What It Says.

--- liri.js
+++ liri.js
@@ -1,17 +1,45 @@
 var dotEnv = require("dotenv").config();
+var fs = require('fs');
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var inquirer = require('inquirer');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var request = require("request");
 
 function tweetCall() {
     console.log('Tweet Call Success');
 }
 
 function spotifyCall() {
-    console.log('Spotify Call Success');
+    inquirer.prompt([
+        {
+            name: 'query',
+            type: 'input',
+            message: 'What is your song query?'
+        },
+        {
+            name: 'artist',
+            type: 'input',
+            message: 'Who is the artist?'
+        }
+    ]).then(function(response) {
+        if(response.query === '') {
+            response.query = 'The Sign';
+            response.artist = 'Ace of Base'
+        };
+        console.log('Spotify Call Success');
+        spotify.request('https://api.spotify.com/v1/search?q=track:' + response.query + '%20artist:' + response.artist + '&limit=1&type=track&market=us').then(function(data) {
+            spotifyResponse = data;
+            console.log('Band: ' + spotifyResponse.tracks.items[0].album.artists[0].name);
+            console.log('Song: ' + spotifyResponse.tracks.items[0].name);
+            console.log('Album: ' + spotifyResponse.tracks.items[0].album.name);
+            console.log('Link: ' + spotifyResponse.tracks.items[0].album.external_urls.spotify);
+        }).catch(function(err) {
+            console.log(err);
+        });
+    })
 }
 
 function movieCall() {
@@ -19,7 +47,22 @@ function movieCall() {
 }
 
 function randomtxtCall() {
-    console.log('Text Call Success');
+    fs.readFile('random.txt', 'utf8', function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(data);
+        var dataArr = data.split(",");
+        spotify.request('https://api.spotify.com/v1/search?q=track:' + dataArr[1] + '&limit=1&type=track&market=us').then(function(data) {
+            spotifyResponse = data;
+            console.log('Band: ' + spotifyResponse.tracks.items[0].album.artists[0].name);
+            console.log('Song: ' + spotifyResponse.tracks.items[0].name);
+            console.log('Album: ' + spotifyResponse.tracks.items[0].album.name);
+            console.log('Link: ' + spotifyResponse.tracks.items[0].album.external_urls.spotify);
+        }).catch(function(err) {
+            console.log(err);
+        });
+    })
 }
 
 inquirer.prompt([
@@ -28,23 +71,18 @@ inquirer.prompt([
         type: 'list',
         message: 'What would you like to do?',
         choices: ['View My Tweets', 'Spotify This Song', 'Movie This', 'Do What it Says']
-    },
-    {
-        name: 'query',
-        type: 'input',
-        message: 'What is your query?',
     }
 ]).then(function(response) {
     if (response.option === 'View My Tweets') {
-        tweetCall(response.query);
+        tweetCall();
     }
     if (response.option === 'Spotify This Song') {
-        spotifyCall(response.query);
+        spotifyCall();
     }
     if (response.option === 'Movie This') {
-        movieCall(response.query);
+        movieCall();
     }
     if (response.option === 'Do What it Says') {
-        randomtxtCall(response.query);
+        randomtxtCall();
     }
 });
