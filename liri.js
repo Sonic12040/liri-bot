d495238cd7da507519bc073cff8d85f182ea6e92
Sonic12040 <Daniel.g.Richmond@gmail.com>
2019-05-26 22:06:43 -0500

    Refactored part of the code. Started replacing console.log with process.stdout.write.

--- liri.js
+++ liri.js
@@ -1,26 +1,20 @@
-var dotEnv = require('dotenv').config();
-var fs = require('fs');
-var keys = require('./keys.js');
-var Twitter = require('twitter');
-var Spotify = require('node-spotify-api');
-var inquirer = require('inquirer');
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-var request = require('request');
+const dotEnv = require('dotenv').config();
+const fs = require('fs');
+const keys = require('./keys.js');
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+const inquirer = require('inquirer');
+const spotify = new Spotify(keys.spotify);
+const client = new Twitter(keys.twitter);
+const request = require('request');
 
 function tweetCall() {
-	console.log('Tweet Call Success');
 	let params = {
 		count: 20
 	};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		if(error) {
-			console.log(error);
-		}
-        var tweetsArr = tweets;
-		tweetsArr.forEach(element => {
-			console.log(element.text);
-		});
+	client.get('statuses/user_timeline', params, function(error, tweets) {
+		if (error) throw new Error;
+		tweets.forEach(tweet => process.stdout.write(tweet.text + '\n'));
 	});
 }
 
@@ -41,21 +35,23 @@ function spotifyCall() {
 			response.query = 'The Sign';
 			response.artist = 'Ace of Base';
 		}
-        console.log('Spotify Call Success');
 		spotify.request('https://api.spotify.com/v1/search?q=track:' + response.query + '%20artist:' + response.artist + '&limit=1&type=track&market=us').then(function(data) {
-			spotifyResponse = data;
-			console.log('Band: ' + spotifyResponse.tracks.items[0].album.artists[0].name);
-			console.log('Song: ' + spotifyResponse.tracks.items[0].name);
-			console.log('Album: ' + spotifyResponse.tracks.items[0].album.name);
-			console.log('Link: ' + spotifyResponse.tracks.items[0].album.external_urls.spotify);
-		}).catch(function(err) {
-			console.log(err);
+			const spotifyResponse = data.tracks.items[0];
+			if (spotifyResponse !== undefined || null) {
+				console.log('Band: ' + spotifyResponse.album.artists[0].name);
+				console.log('Song: ' + spotifyResponse.name);
+				console.log('Album: ' + spotifyResponse.album.name);
+				console.log('Link: ' + spotifyResponse.album.external_urls.spotify);
+			} else {
+				process.stdout.write('No response found.');
+			}
+		}).catch(function(error) {
+			process.stdout.write(error);
 		});
 	});
 }
 
 function movieCall() {
-	console.log('Movie Call Success');
 	inquirer.prompt([
 		{
 			name: 'query',
@@ -63,7 +59,7 @@ function movieCall() {
 			message: 'What movie are you searching for?'
 		}
 	]).then(function(response) {
-		query = response.query;
+		let query = response.query;
 		if(query === '') {
 			query = 'Mr Nobody';
 		}
@@ -71,7 +67,7 @@ function movieCall() {
 			if(error) {
 				console.log(error);
 			}
-            console.log(JSON.parse(body).Title);
+			console.log(JSON.parse(body).Title);
 			console.log(JSON.parse(body).Year);
 			console.log(JSON.parse(body).imdbRating);
 			console.log(JSON.parse(body).Ratings[1].Value);
@@ -84,18 +80,16 @@ function movieCall() {
 }
 
 function randomtxtCall() {
-	fs.readFile('random.txt', 'utf8', function(err, data) {
-		if (err) {
-			return console.log(err);
-		}
+	fs.readFile('random.txt', 'utf8', function(error, data) {
+		if (error) throw new Error;
 		console.log(data);
 		var dataArr = data.split(',');
 		spotify.request('https://api.spotify.com/v1/search?q=track:' + dataArr[1] + '&limit=1&type=track&market=us').then(function(data) {
-			spotifyResponse = data;
-			console.log('Band: ' + spotifyResponse.tracks.items[0].album.artists[0].name);
-			console.log('Song: ' + spotifyResponse.tracks.items[0].name);
-			console.log('Album: ' + spotifyResponse.tracks.items[0].album.name);
-			console.log('Link: ' + spotifyResponse.tracks.items[0].album.external_urls.spotify);
+			const spotifyResponse = data.tracks.items[0];
+			console.log('Band: ' + spotifyResponse.album.artists[0].name);
+			console.log('Song: ' + spotifyResponse.name);
+			console.log('Album: ' + spotifyResponse.album.name);
+			console.log('Link: ' + spotifyResponse.album.external_urls.spotify);
 		}).catch(function(err) {
 			console.log(err);
 		});
